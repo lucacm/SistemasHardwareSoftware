f8253586370f5977ad62d830adfa67eaafbf2e8b
Igor Montagner <igordsm@gmail.com>
2021-04-08 17:23:49 -0300

    Deployed be2a530 with MkDocs version: 1.1.2

--- aulas/10-malloc/ex1.c
+++ aulas/10-malloc/ex1.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 14
+#define N 12
 
-int main(int argc, char *argv[]) {
-    int *vetor = malloc(N * sizeof(int));
-    int i;
+int *aloca_vetor(int n) {
+    int *vetor = malloc(n * sizeof(int));
+    return vetor;
+}
 
-    for (i = 0; i <= N; i++) {
+void atribui(int *vetor, int n) {
+    for (int i = 0; i <= n; i++) { // problema!
         vetor[i] = i;
     }
+}
 
-    for (i = 0; i <= N; i++) {
+int main(int argc, char *argv[]) {
+    int *vetor = aloca_vetor(N);
+    int i;
+
+    atribui(vetor, N);
+
+    for (i = 0; i <= N; i++) { // problema!
         printf("Elemento %d: %d\n", i+1, vetor[i]);
     }
 
