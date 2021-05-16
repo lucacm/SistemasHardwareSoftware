97374955a327b43e11513e5354b470e37ff4f180
Igor Montagner <igordsm@gmail.com>
2021-05-16 16:32:57 -0300

    Deployed 9a83d49 with MkDocs version: 1.1.2

--- aulas/17-sinais-I/parte1.c
+++ aulas/17-sinais-I/parte1.c
@@ -1,14 +1,28 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
 
 int main() {
     pid_t filho;
 
     filho = fork();
     if (filho == 0) {
-        int i = 1/0;
-        printf("Divisão por zero!\n");
+        printf("Meu pid: %d\n", getpid());
+        while(1);
     }
+
+    int ws;
+    wait(&ws);
+    printf("EXITED %d \n"
+           "SIGNALED %d \n"
+           "TERMSIG %d\n"
+           "Error: %s\n",
+           WIFEXITED(ws), 
+           WIFSIGNALED(ws),
+           WTERMSIG(ws), 
+           strsignal(WTERMSIG(ws)));
 
     return 0;
 }
