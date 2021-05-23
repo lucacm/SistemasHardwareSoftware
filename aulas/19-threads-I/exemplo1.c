20f14c441e85e45def7c67461ff822caa81cc733
igor montagner <igordsm@gmail.com>
2021-05-23 15:20:58 -0300

    Deployed f0d8d71 with MkDocs version: 1.1.2

--- aulas/19-threads-I/exemplo1.c
+++ aulas/19-threads-I/exemplo1.c
@@ -1,21 +1,37 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int i1, i2;
+    int ret;
+} args_t;
 
 void *minha_thread(void *arg) {
-    printf("Hello thread!\n");
+    args_t *args = (args_t *) arg;
+    printf("i1: %d i2: %d\n", args->i1, args->i2);
+    args->ret = args->i2 + args->i1;
     return NULL;
 }
 
-
 int main() {
-    pthread_t tid;
+    pthread_t tids[4];
+    args_t *args = malloc(sizeof(args_t) * 4);
     
-    int error = pthread_create(&tid, NULL, minha_thread, NULL);
 
-    printf("Hello main\n");
-    
-    pthread_join(tid, NULL);
+    for (int i = 0; i < 4; i++) {
+        args[i].i1 = i * 2;
+        args[i].i2 = i * 3;
+        pthread_create(&tids[i], NULL, minha_thread, &args[i]);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        pthread_join(tids[i], NULL);
+        printf("Ret: %d\n", args[i].ret);
+    }
+
+    free(args);
     
     return 0;
 }
